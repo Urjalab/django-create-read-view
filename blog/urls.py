35127824157a6916853c283366b7dec2810981b2
thearjun <be2017se573@gces.edu.np>
2020-12-17 13:54:29 +0545

    Add update view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import read_view, create_view
+from .views import read_view, create_view, update_view
 
 urlpatterns = [
     path('', read_view),
     path('create/', create_view),
+    path('update/<int:post_id>/', update_view),
 ]
--- blog/views.py
+++ blog/views.py
@@ -51,3 +51,43 @@ def create_view(request):
         'blog/create.html',
         context
     )
+
+
+def update_view(request, post_id):
+
+    if request.method == 'GET':
+
+        post_obj = Post.objects.get(id=post_id) # exception generate
+        form = PostForm(instance=post_obj)
+
+        context = {
+            'form': form
+        }
+
+        return render(
+            request,
+            'blog/update.html',
+            context
+        )
+
+    elif request.method == 'POST':
+
+        form_data = PostForm(request.POST, request.FILES)
+
+        if form_data.is_valid():
+
+            # https://stackoverflow.com/a/12848678/9755816
+
+            post_model_obj = form_data.cleaned_data # dictionary
+
+            post_model_obj = form_data.save(commit=False)
+            old_post_model_obj = Post.objects.get(id=post_id)
+            old_post_model_obj.title = post_model_obj.title            
+            old_post_model_obj.content = post_model_obj.content  
+            if post_model_obj.image is not None:
+                old_post_model_obj.image =   post_model_obj.image         
+            old_post_model_obj.save()
+
+            return redirect('/')
+        else:
+            print(form_data.errors)
